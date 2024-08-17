2a462195960b3a44e9593a9f54432f982f282f7e
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2024-08-18 01:12:22 +0700

    init project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementX;
+
+    private Rigidbody2D myBody;
+
+    private SpriteRenderer sr;
+
+    private Animator anim;
+    private string WALK_ANIMATION = "Walk";
+
+    private bool isGrounded = true;
+    private string GROUND_TAG = "Ground";
+
+
+    void Awake()
+    {
+
+        myBody = GetComponent<Rigidbody2D>();
+
+        sr = GetComponent<SpriteRenderer>();
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+        PlayerJump();
+    }
+
+
+    private void FixedUpdate()
+    {
+        //PlayerJump();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementX, 0f, 0f) * moveForce * Time.deltaTime;
+    }
+
+    void AnimatePlayer()
+    {
+        if(movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+
+
+        } else if(movementX < 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+
+        } else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+
+    void PlayerJump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag(GROUND_TAG))
+        {
+            isGrounded = true;
+            Debug.Log("isGrounded : " + isGrounded);
+        }
+    }
+}
